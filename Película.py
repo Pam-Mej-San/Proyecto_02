9d26b25ab8d2a8847fa3d9173b5d9160d11f9c64
Kiwisan32 <pamrms01@gmail.com>
2024-10-02 14:34:59 -0600

    Agregando archivo

--- Película.py
+++ Película.py
@@ -4,25 +4,25 @@
 
 class Película:
     def __init__(self, título, director, año):
-        self.título=título 
-        self.director=director
-        self.año=año 
+        self.título=título  #Atributo título
+        self.director=director #Atributo director
+        self.año=año  #Atributo año
 
 #Notas: Atributos privados y 
 #definir un método para acceder al nombre de la película (getter).
     @property
-    def título(self):
+    def título(self): #Método para título
         return self.__título
     
     @property
-    def director (self):
+    def director (self): #Método para director
         return self.__director
     
     @property
     def año(self):
-        return self.__año
+        return self.__año #método para año
 
-    #Para que se vea la información de la película en linea o cadena   
+    #Método para que se vea la información de la película en linea o cadena   
     def __str__(self):
         return f'{self.título}, dirigida por {self.director}, año {self.año}'
     
