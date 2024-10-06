df25796148a106d049b75c3e1a2efc9a15bc53c7
Kiwisan32 <pamrms01@gmail.com>
2024-10-06 16:59:03 -0600

    Agregar archivo

--- Catálogo.py
+++ Catálogo.py
@@ -1,19 +1,28 @@
-#Implementación de la clase CatalogoPelicula
+#Implementación de la clase CatálogoPelicula
 #Esta es la clase que va a gestionar todo el catálogo de películas
-# Gestionar operaciones como agregar, listar y eliminar películas del catálogo.
-# Atributos: nombre: El nombre del catálogo y 
-# ruta_archivo: La ruta del archivo donde se va a guardar el catálogo.
+#Gestionar operaciones como agregar, listar y eliminar películas del catálogo.
+#Atributos: nombre: El nombre del catálogo y 
+#ruta_archivo: La ruta del archivo donde se va a guardar el catálogo.
 
 
 class CatálogoPelícula:
     #Constructor
-    def __init__(self, nombre_catálogo): 
-        self.nombre= nombre_catálogo #Atributo
-        self.ruta_archivo= f'{nombre_catálogo}.txt' #Debería abrir un archivo con el atributo
-
+    def __init__(self, título_catálogo): 
+        self.nombre= título_catálogo #Atributo
+        self.ruta_archivo= f'{título_catálogo}.txt' #Debería abrir un archivo con el atributo
 
 #Método agregar 'add' open(self.ruta_archivo, 'a')
+    def agregar(self,película):  #Agregar
+        with open(self.ruta_archivo, 'a') as file: #Se crea y agrega 
+            file.write(f'{película.título}, {película.director}, {película.año} \n') #imprimen los detalles de la película agregada
+
+    def listar(self): #listar
+        try:
+    
+    def eliminar(self): #eliminar
+        
+
 #lo mismo con lo demás: listar, elminar, etc.
 
-catalogo = CatálogoPelícula("MisPeliculas")
+catálogo = CatálogoPelícula('MisPeliculas')
 print(catalogo)
